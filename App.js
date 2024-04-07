86a986d10a4a9983a4c948e29917cccfe8376d1b
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-07 20:24:54 +0200

    EP3 Components

--- App.js
+++ App.js
@@ -1,35 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * <div id="parent">
- *      <div id ="child">
- *              <h1>this is a h1 tag </h1>
- *      <div>
- *  <div>
- */
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Namaste React 🚀"),
-    React.createElement("h2", {}, "This is a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "This is a h1 tag"),
-    React.createElement("h2", {}, "This is a h2 tag"),
-  ]),
-]);
+//React.createElement => ReactObject-JS Object => HTMLElement(render)
 
-console.log(parent);
+//JSX - HTML-like or XML-like syntax
+
+//JSX (transpiled before it reaches the JS) - PARCEL - Babel
+
+//JSX => babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+//React Element
+const heading = (
+  <h1 className="heading" tabIndex="1">
+    Namaste React Using JSX 🚀
+  </h1>
+);
+
+//React Component
+
+const HeadingComponent = () => <h1>Namaste React Functional Component</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-
-// console.log(heading);
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World from React!"
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-// console.log(heading);
