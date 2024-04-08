e2903ee0d0c0aa550bd144b694ea381ba79f2391
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-08 17:31:39 +0200

    EP4 Food ordering app layout

--- App.js
+++ App.js
@@ -1,24 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-//React.createElement => ReactObject-JS Object => HTMLElement(render)
+/**
+ * Header
+ * - Logo
+ * - New Items
+ * Body
+ * - Search
+ * - Restaurant Container
+ *    - Img
+ *    - Name of Res, Star Rating, Cuisine, delivery time
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ * */
 
-//JSX - HTML-like or XML-like syntax
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://banner2.cleanpng.com/20180702/vrt/kisspng-online-food-ordering-food-delivery-grubhub-5b3a1b75a36132.9626073015305347736692.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home </li>
+          <li>About Us </li>
+          <li>Contact Us </li>
+          <li>Cart </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//JSX (transpiled before it reaches the JS) - PARCEL - Babel
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghna Foods</h3>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">{/* RestaurantCard */}</div>
+    </div>
+  );
+};
 
-//JSX => babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-//React Element
-const heading = (
-  <h1 className="heading" tabIndex="1">
-    Namaste React Using JSX 🚀
-  </h1>
-);
-
-//React Component
-
-const HeadingComponent = () => <h1>Namaste React Functional Component</h1>;
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<App />);
