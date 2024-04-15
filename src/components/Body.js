30b023ac01bde611185619c4b45e81930389d776
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-15 18:12:35 +0200

    feat: added a button on home page

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,16 @@ import resList from "../utils/mockdata";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("Button CLicked ");
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
