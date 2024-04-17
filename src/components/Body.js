0346bb0c89d288694269cc0a7ecfc09225ef9a4c
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-17 20:07:26 +0200

    fix: remove mock data and render data dynamically using live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockdata";
+import { useState, useEffect } from "react";
 
 //not using keys (not acceptable) <<< index as keys <<<<<< unique id (Best)
 
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListofRestaurant] = useState(resList);
+  const [listOfRestaurants, setListofRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setListofRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    console.log(json);
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <h1>Loading... </h1>;
+  }
 
   return (
     <div className="body">
@@ -15,7 +35,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.5
+              (res) => res.info.avgRating > 4.0
             );
             setListofRestaurant(filteredList);
           }}
