8a031818a1167b4f1550424461a5931d3b1662e0
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-19 13:58:37 +0200

    feat: Add react-router-dom (routing)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "../index.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -13,5 +17,21 @@ const App = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
