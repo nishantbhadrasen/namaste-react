d053485ac2f0485261ee006409fe30fcd4455a96
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-20 17:05:10 +0200

    refactor: Add Menu_API and Restaurant_List

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,9 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://banner2.cleanpng.com/20180702/vrt/kisspng-online-food-ordering-food-delivery-grubhub-5b3a1b75a36132.9626073015305347736692.jpg";
+
+export const MENU_API =
+  "https://corsproxy.io/https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=";
+
+export const RESTAURANT_LIST =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
