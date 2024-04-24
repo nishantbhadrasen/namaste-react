4638979f15c36917e9eab971bac2a5c35d101028
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-24 10:59:21 +0200

    feat: Add taiwlind into project

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,6 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
+    <div className="shimmer-container m-4 p-4 bg-gray-50 rounded-lg">
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
