7734c3804afd012e30ca077f3b57275e0917ea13
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-26 14:15:57 +0200

    feat: Display Restaurant list and Restaurant Items on the RestaurantMenu page

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,34 +2,32 @@ import React from "react";
 import Shimmer from "./Shimmer";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import { useParams } from "react-router-dom";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
-  const { resInfo, recommended } = useRestaurantMenu(resId);
+  
+  const { resInfo, recommended, categories } = useRestaurantMenu(resId);
 
   if (!resInfo) return <Shimmer />;
+  //console.log(categories);
 
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[2]?.card?.card?.info;
 
   return (
-    <div className="menu">
-      {name && (
-        <>
-          <h1>{name}</h1>
-          <p>{cuisines?.join(", ")}</p>
-          <h3>{costForTwoMessage}</h3>
-        </>
-      )}
-      <h2>Menu</h2>
-      <ul>
-        {recommended?.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} -{" ₹ "}
-            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-          </li>
-        ))}
-      </ul>
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines?.join(", ")} - {costForTwoMessage}
+      </p>
+      {/* Render categories using the RestaurantCategory component */}
+      {categories.map((category) => (
+        <RestaurantCategory
+          key={category?.card?.card?.title}
+          data={category?.card?.card}
+        />
+      ))}
     </div>
   );
 };
