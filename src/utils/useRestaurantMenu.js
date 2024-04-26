d7394916dd615236dd304302c4c71816078fdbcd
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-26 14:16:35 +0200

    fix: Categories Data bug fix

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -4,6 +4,7 @@ import { MENU_API } from "../utils/constants";
 const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState(null);
   const [recommended, setRecommended] = useState(null);
+  const [categories, setCategories] = useState(null); // Define categories state
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,16 +14,25 @@ const useRestaurantMenu = (resId) => {
         resJson.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.find(
           (obj) => obj.card?.card?.type === "CATEGORY_TYPE_RECOMMENDED"
         );
+
+      const categoriesData =
+        resJson.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+          (c) =>
+            c?.card?.card?.["@type"] ===
+            "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+        );
+
       const recommendedItems = allCards?.card?.card?.itemCards;
 
-      setResInfo(resJson.data); // Ensure that we set the whole data, not just resJson
+      setResInfo(resJson.data); // Ensure that we set the whole data, not just parts of it
       setRecommended(recommendedItems);
+      setCategories(categoriesData); // Set the categories from fetched data
     };
 
     fetchData();
   }, [resId]); // Include resId in the dependency array
 
-  return { resInfo, recommended };
+  return { resInfo, recommended, categories };
 };
 
 export default useRestaurantMenu;
