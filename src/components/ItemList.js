e1f228f80a752f27ff79b15e526d531665aab417
Nishant <de.nishantbhadrasen@gmail.com>
2024-04-28 23:49:48 +0200

    feat: Add Handler for Redux to Subscribe the Items

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,14 @@
+import { useDispatch } from "react-redux";
 import { CDN_URL } from "../utils/constants";
+import { addItem } from "../utils/cartSlice";
 const ItemList = ({ items }) => {
-  
+  const disptach = useDispatch();
+
+  const handleAddItem = (item) => {
+    //Disptach an action
+    disptach(addItem(item));
+  };
+
   return (
     <div>
       {items.map((item) => (
@@ -22,7 +30,10 @@ const ItemList = ({ items }) => {
           </div>
           <div className="w-3/12 p-4 ">
             <div className="absolute">
-              <button className="p-2 mx-16 rounded-lg  bg-black text-white shadow-lg">
+              <button
+                className="p-2 mx-16 rounded-lg  bg-black text-white shadow-lg"
+                onClick={() => handleAddItem(item)}
+              >
                 ADD +
               </button>
             </div>
